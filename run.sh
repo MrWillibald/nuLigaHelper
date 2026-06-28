500acf29e899981348162b475d86b037ddd0e0af
Manuel Rösler <manuel.roesler@tum.de>
2026-06-28 20:41:00 +0200

    add run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the optimizer script
+python nuLigaHelperClass.py
+
+# Deactivate the virtual environment
+deactivate
